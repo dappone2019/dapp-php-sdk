5de0a5e348f09ab96b1a7e5d715744281f44de97
dappone <dappone2019@hotmail.com>
2019-09-26 16:11:10 +0800

    webhook
    demo

--- README.md
+++ README.md
@@ -3,4 +3,26 @@
 
 ## RSA签名工具
 
-> php RsaTool.php 
+> php RsaTester.php
+
+## webhook 伪代码
+
+cd ${dapp-php-sdk} 即 当前工作目录
+
+> php -S localhost:8000 webhook.php
+
+- 支付回调
+
+`http://localhost:8000/pay_notify`
+
+- 查询用户积分
+
+`http://localhost:8000/get_point`
+
+- 扣除用户积分
+
+`http://localhost:8000/reduce_point`
+
+- 扣除用户通知（不成功会发送通知）
+
+`http://localhost:8000/reduce_point_notify`
--- RsaTool.php
+++ RsaTool.php
@@ -1,36 +1,5 @@
 <?php
 
-$public_key_path = dirname(__FILE__) . '/key/rsa_public_key.pem';
-
-// 支付成功回调的参数
-$params = array(
-  'app_id' => 1,
-  'out_trade_no' => '5621048635828989',
-  'status' => 4,
-  'createtime' => 1555047944,
-  'amount' => '3',
-  'real_amount' => '3',
-  'confirm_time' => 1555051016,
-  'openid' => 'hFXkZSdnrEj7QuN8xDYg9G',
-  '_signature' => 'wRfPt61g6fxrk13aoZL3gvMuW6lnncjFNlg9RDA0ftCYh1edBvoqE6slQYaqruijT/zqaDhywz9zVt636TcjN96NJLIfoFSsEU/5udbnYnfbeCNrzJzfz7KOZDaY7HmEAuq2hybqjGO4wiuCakeiENsYUbCP6ySQpTZvQcs+Iec=',
-);
-// 开发者默认接收 post 请求参数
-// $params = $_POST;
-
-$_signature = $params['_signature'];
-unset($params['_signature']);
-$data = param2str($params);
-$pubKey = file_get_contents($public_key_path);
-$res = openssl_get_publickey($pubKey);
-$result = (bool)openssl_verify($data, base64_decode($_signature), $res);
-openssl_free_key($res);
-
-if($result){
-  echo 'verify pass ~'.PHP_EOL;
-}else{
-  echo 'verify error ~'.PHP_EOL;
-}
-
 function param2str($params)
 {
     if (is_array($params)) {
@@ -47,4 +16,20 @@ function param2str($params)
     }
 
     return $string;
+}
+
+function verifySignatureRSA($params, $_signature)
+{
+  $public_key_path = dirname(__FILE__) . '/key/rsa_public_key.pem';
+  unset($params['_signature']);
+  $data = param2str($params);
+  $pubKey = file_get_contents($public_key_path);
+  $res = openssl_get_publickey($pubKey);
+  $result = (bool)openssl_verify($data, base64_decode($_signature), $res);
+  openssl_free_key($res);
+  if($result){
+    return true;
+  }else{
+    return false;
+  }
 }
